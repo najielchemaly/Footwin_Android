a96a0db04a74dfa3ac33648d6a2d1437ae0bade2
NajiElChemaly <naji.chemaly@intouchmena.com>
2018-03-26 22:46:19 +0300

    init

---
+++ app/src/main/java/com/apploads/footwin/MainActivity.java
@@ -0,0 +1,13 @@
+package com.apploads.footwin;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
