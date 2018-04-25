033fc327a756e10ff18ecd392e309b5c1061df9b
Charbel Chidiac <cchidiac@app-loads.com>
2018-04-25 10:11:20 +0300

    Added notification classes, Leaderboard fixses

--- app/src/main/java/com/apploads/footwin/firebase/FirebaseIdService.java
+++ app/src/main/java/com/apploads/footwin/firebase/FirebaseIdService.java
@@ -1,4 +1,47 @@
 package com.apploads.footwin.firebase;
 
-public class FirebaseIdService {
+import android.util.Log;
+
+import com.apploads.footwin.services.ApiManager;
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class FirebaseIdService extends FirebaseInstanceIdService {
+    private static final String TAG = "FirebaseIDService";
+
+    @Override
+    public void onTokenRefresh() {
+        // Get updated InstanceID token.
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+        Log.d(TAG, "Refreshed token: " + refreshedToken);
+
+        // TODO: Implement this method to send any registration to your app's servers.
+        sendRegistrationToServer(refreshedToken);
+    }
+
+    /**
+     * Persist token to third-party servers.
+     *
+     * Modify this method to associate the user's FCM InstanceID token with any server-side account
+     * maintained by your application.
+     *
+     * @param token The new token.
+     */
+    private void sendRegistrationToServer(String token) {
+        ApiManager.getService().updateFirebaseToken(token).enqueue(new Callback<Object>() {
+            @Override
+            public void onResponse(Call<Object> call, Response<Object> response) {
+
+            }
+
+            @Override
+            public void onFailure(Call<Object> call, Throwable t) {
+
+            }
+        });
+    }
 }
